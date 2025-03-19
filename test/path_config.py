54f3a2ae227fc6207e195018fbc2459fbcc6bd3e
VladPetac <vladalexandru2012@yahoo.com>
2025-03-19 20:21:22 +0200

    Changed path_config so it will match the correct project folder

--- test/path_config.py
+++ test/path_config.py
@@ -1,3 +1,3 @@
 import sys
 #Adauga path-ul complet catre folderul proiectului tau (foloseste /)
-sys.path.append("E:/Python/Curs Python + Selenium/Tema_finala")
+sys.path.append("E:/Python/Curs Python + Selenium/finalHomework_Udemy/")
